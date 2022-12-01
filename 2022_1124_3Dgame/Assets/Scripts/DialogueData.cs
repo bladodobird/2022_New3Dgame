4b3394566dd922fad5dc8eab4fe207591510dec6
bladodobird <113589179+bladodobird@users.noreply.github.com>
2022-12-01 15:14:54 +0800

    加對話框

---
+++ 2022_1124_3Dgame/Assets/Scripts/DialogueData.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace YIZU
+{
+    /// <summary>
+    /// 對話資料
+    /// </summary>
+    [CreateAssetMenu(menuName = "YIZU/Dialogue Data", fileName = "New Dialogue Data")]
+    public class DialogueData : ScriptableObject
+    {
+        [Header("對話者名")]
+        public string dialogueName;
+        [Header("對話者內容"), TextArea(2, 10)]
+        public string[] dialogueContents;
+    }
+}
