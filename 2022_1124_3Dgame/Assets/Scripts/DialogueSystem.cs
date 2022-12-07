177372df8f5cebb5bcab809710c25688535237cb
bladodobird <109051069@live.asia.edu.tw>
2022-12-07 20:05:25 +0800

    對話系統

--- 2022_1124_3Dgame/Assets/Scripts/DialogueSystem.cs
+++ 2022_1124_3Dgame/Assets/Scripts/DialogueSystem.cs
@@ -1,19 +1,36 @@
+﻿using UnityEngine;
 using TMPro;
-using UnityEngine;
 
 namespace YIZU
 {
     /// <summary>
-    /// ��ܨt��
+    /// 對話系統
     /// </summary>
     public class DialogueSystem : MonoBehaviour
     {
-        [SerializeField, Header("��ܶ��j"), Range(0, 0.5f)]
+        #region 資料區
+        [SerializeField, Header("對話間隔"), Range(0, 0.5f)]
         private float dialogueIntervalTime = 0.1f;
+        [SerializeField, Header("開頭動畫")]
+        private DialogueData dialogueOpening;
 
-        private WaitForSeconds dialogueInterval;
+        private WaitForSeconds dialogueInterval => new WaitForSeconds(dialogueIntervalTime);
+
         private CanvasGroup groupDialogue;
+        private TextMeshProUGUI textName;
         private TextMeshProUGUI textContent;
         private GameObject goTriangle;
+        #endregion
+
+        #region 事件
+        private void Awake()
+        {
+            groupDialogue = GameObject.Find("畫布對話系統").GetComponent<CanvasGroup>();
+            textName = GameObject.Find("對話者名").GetComponent<TextMeshProUGUI>();
+            textContent = GameObject.Find("對話者台詞").GetComponent<TextMeshProUGUI>();
+            goTriangle = GameObject.Find("對話結束圖示");
+            goTriangle.SetActive(false);
+        } 
+        #endregion
     }
 }
