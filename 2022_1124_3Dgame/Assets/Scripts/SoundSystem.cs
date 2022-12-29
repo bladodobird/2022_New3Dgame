2243785d6a64819c91190e930fdf3454fc90f0f1
bladodobird <113589179+bladodobird@users.noreply.github.com>
2022-12-29 15:30:35 +0800

    音效系統

---
+++ 2022_1124_3Dgame/Assets/Scripts/SoundSystem.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace YIZU
+{
+    /// <summary>
+    /// 音效系統
+    /// </summary>
+    [RequireComponent(typeof(AudioSource))]
+    public class SoundSystem : MonoBehaviour
+    {
+
+        private AudioSource aud;
+
+        private void Awake()
+        {
+            aud = GetComponent<AudioSource>();
+        }
+
+        /// <summary>
+        /// 播放音效
+        /// </summary>
+        /// <param name="sound">要撥放的音效</param>
+        public void PlaySound(AudioClip sound)
+        {
+            // 音效來源,播放一次音效(音效);
+            aud.PlayOneShot(sound);
+        }
+        
+    }
+}
